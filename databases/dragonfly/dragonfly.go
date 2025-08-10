595064b153bc805e4549a06c170896b22515c992
Sercan REYHANLI <sercan@teamlify.co.uk>
2025-08-11 00:50:45 +0100

    feat: add dragonfly database support

---
+++ databases/dragonfly/dragonfly.go
@@ -0,0 +1,30 @@
+package redis
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/redis/go-redis/v9"
+	cfg "github.com/spf13/viper"
+)
+
+// NewDFClient Returns new DragonFly (using same driver with redis) client
+func NewDFClient(ctx context.Context, dbNum int) (db *redis.Client, err error) {
+
+	redisHost := fmt.Sprintf("%s:%d", cfg.GetString("Dragonfly.HOST"), cfg.GetInt("Dragonfly.PORT"))
+
+	db = redis.NewClient(&redis.Options{
+		Addr:         redisHost,
+		Username:     cfg.GetString("Dragonfly.USER"),
+		Password:     cfg.GetString("Dragonfly.PASS"),
+		MinIdleConns: cfg.GetInt("Dragonfly.MIN_IDLE_CONN"),
+		PoolSize:     cfg.GetInt("Dragonfly.POOL_SIZE"),
+		PoolTimeout:  time.Duration(cfg.GetInt("Dragonfly.POOL_TIMEOUT")),
+		DB:           dbNum,
+	})
+
+	_, err = db.Ping(ctx).Result()
+
+	return
+}
