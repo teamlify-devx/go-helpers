9008eba3f21cc97bfa8f9f1f8facbd042449b4b9
Sercan REYHANLI <sercan@teamlify.co.uk>
2026-01-03 15:44:04 +0300

    change: config model param converted to optional

--- config/config.go
+++ config/config.go
@@ -8,7 +8,7 @@ import (
 
 /*
 ParseConfig Config parser function
-  - [conf] : the structure of your config file
+  - [conf] : the structure of your config file (optional)
   - [type_of] : the type of config file (yaml, env, json etc)
   - [file_path] : the path of your config file
   - [file_name] : the file name of your config file
@@ -25,10 +25,12 @@ func ParseConfig(conf interface{}, type_of, file_path, file_name string) (err er
 		return err
 	}
 
-	err = viper.Unmarshal(&conf)
-	if err != nil {
-		log.Printf("Unable to decode config file into struct, %v \n", err)
-		return err
+	if conf != nil {
+		err = viper.Unmarshal(&conf)
+		if err != nil {
+			log.Printf("Unable to decode config file into struct, %v \n", err)
+			return err
+		}
 	}
 
 	return nil
